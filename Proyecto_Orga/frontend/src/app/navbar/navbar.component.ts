2b8a5d76eb10b428500102163a302f4f89292602
MauGarr <3292719381105@ingenieria.usac.edu.gt>
2024-05-02 10:56:59 -0600

    Se agrega componente Navbar

---
+++ Proyecto_Orga/frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styles: ``
+})
+
+export class NavbarComponent {
+
+  ayuda() {
+    window.open('assets/pdf/DOCUMENTACION.pdf', '_blank');
+  } 
+}
